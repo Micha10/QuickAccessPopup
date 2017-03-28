8f211399da9df57a4ead07233f78f109f6522fc2
Jean Lalonde <courriel@jeanlalonde.ca>
2017-03-28 15:00:32 -0400

    v8.1.9.5 beta ready

--- Build-v8-beta/Compile-QAP-BATCH-beta.bat
+++ Build-v8-beta/Compile-QAP-BATCH-beta.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 rem Set environment variables for Inno Setup and this batch
-SET QAPVERSIONPREV=8_1_9_3
-SET QAPVERSION=8_1_9_4
-SET QAPVERSIONTEXT=v8.1.9.4 BETA
+SET QAPVERSIONPREV=8_1_9_4
+SET QAPVERSION=8_1_9_5
+SET QAPVERSIONTEXT=v8.1.9.5 BETA
 SET QAPBETAPROD=-beta
 rem Call Compile batch
 CALL "C:\Dropbox\AutoHotkey\QuickAccessPopup\Setup Script files\Compile-QAP-v8.bat"
