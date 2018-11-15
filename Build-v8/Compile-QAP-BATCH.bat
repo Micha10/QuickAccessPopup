0a6643a9a232d95fdcc5b659abc99d03f69ace33
Jean Lalonde <courriel@jeanlalonde.ca>
2018-11-15 10:42:42 -0500

    gitignore review

---
+++ Build-v8/Compile-QAP-BATCH.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+rem Set environment variables for Inno Setup and the batch Compile-QAP-v8.bat
+SET QAPVERSIONPREV=9_2_2
+SET QAPVERSION=9_3
+SET QAPVERSIONNUMBER=9.3
+SET QAPVERSIONTEXT=v%QAPVERSIONNUMBER%
+SET QAPBETAPROD=
+rem Call Compile batch
+CALL "E:\Dropbox\AutoHotkey\QuickAccessPopup\Setup Script files\Compile-QAP-v8.bat"
