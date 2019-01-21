c4d4edcdd39d840f8280548f1992b4aef31146da
Jean Lalonde <courriel@jeanlalonde.ca>
2019-01-21 13:31:08 -0500

    remove compile batches

--- Build-v8-beta/Compile-QAP-BATCH-beta.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-rem Set environment variables for Inno Setup and this batch
-SET QAPVERSIONPREV=9_3_2_9_6
-SET QAPVERSION=9_4_0_1
-SET QAPVERSIONNUMBER=9.4.0.1
-SET QAPVERSIONTEXT=v%QAPVERSIONNUMBER% BETA
-SET QAPBETAPROD=-beta
-rem Call Compile batch
-CALL "E:\Dropbox\AutoHotkey\QuickAccessPopup\Setup Script files\Compile-QAP-v8.bat"
